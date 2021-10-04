52bfd8a13b4ba25a38522e1788dd9fe707f5a7a7
tvergara <tverg419@gmail.com>
2021-10-03 21:59:14 -0400

    fixing axios call

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://pyploto.herokuapp.com/api/',
+    baseURL: 'https://pyploto-db.herokuapp.com/api/',
     timeout: 5000,
     headers: {
         "Authorization": "JWT " + localStorage.getItem('access_token'),
@@ -10,29 +10,29 @@ const axiosInstance = axios.create({
     }    
 })
 
-axiosInstance.interceptors.response.use(response => response,
-    error => {
-        const originalRequest = error.config
-        if (error.response.status === 401 && error.response.statusText === 'Unauthorized') {
-            return axiosInstance
-            .post('/token/refresh/', {
-                'refresh': localStorage.getItem('refresh_token')
-            })
-            .then(res => {
-                localStorage.setItem('access_token', res.data.access)
-                localStorage.setItem('refresh_token', res.data.refresh)
-                axiosInstance.defaults.headers['Authorization'] = `JWT ${res.data.access}`
+// axiosInstance.interceptors.response.use(response => response,
+//     error => {
+//         const originalRequest = error.config
+//         if (error.response.status === 401 && error.response.statusText === 'Unauthorized') {
+//             return axiosInstance
+//             .post('/token/refresh/', {
+//                 'refresh': localStorage.getItem('refresh_token')
+//             })
+//             .then(res => {
+//                 localStorage.setItem('access_token', res.data.access)
+//                 localStorage.setItem('refresh_token', res.data.refresh)
+//                 axiosInstance.defaults.headers['Authorization'] = `JWT ${res.data.access}`
 
-                originalRequest['Authorization'] = `JWT ${res.data.access}`
+//                 originalRequest['Authorization'] = `JWT ${res.data.access}`
 
-                return axiosInstance(originalRequest)
-            })
-            .catch(err => {
-                console.log(err)
-            })
-        }
-        return Promise.reject(error)
-    }
+//                 return axiosInstance(originalRequest)
+//             })
+//             .catch(err => {
+//                 console.log(err)
+//             })
+//         }
+//         return Promise.reject(error)
+//     }
 
-)
+// )
 export default axiosInstance;
